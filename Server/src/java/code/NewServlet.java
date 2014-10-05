9e0c79dbf23c7807aab891a1c8bd8a70ea9ecb02
Johannes <Johannes@GoDesity>
2014-10-05 14:20:12 +0200

    Added ore page views. Sample data for events in index and page .xhtml Styled intpus textboxes Added logo + link modifikations

---
+++ Server/src/java/code/NewServlet.java
@@ -0,0 +1,102 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package code;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.net.URL;
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+import javax.json.JsonString;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Johannes
+ */
+@WebServlet(name = "DishData", urlPatterns = {"/DishData"})
+public class NewServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/json;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            URL url = new URL("http://ip.jsontest.com/");
+            try  {
+                InputStream is = url.openStream();
+                JsonReader rdr = Json.createReader(is);
+                JsonObject obj = rdr.readObject();
+                JsonString str = obj.getJsonString("ip");
+                out.print(str);
+                out.println(obj.toString());
+                /*JsonArray results = obj.getJsonArray("data");
+                for (JsonObject result : results.getValuesAs(JsonObject.class)) {
+                    out.print(result.getJsonObject("from").getString("name"));
+                    out.print(": ");
+                    out.println(result.getString("message", ""));
+                    out.println("-----------");
+                }*/
+            }catch(Exception e) {}
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
--- Server/src/java/service/ApplicationConfig.java
+++ Server/src/java/service/ApplicationConfig.java
@@ -29,7 +29,6 @@ public class ApplicationConfig extends Application {
      * If required, comment out calling this method in getClasses().
      */
     private void addRestResourceClasses(Set<Class<?>> resources) {
-        resources.add(code.beans.WebsiteMenuBar.class);
         resources.add(service.NewEntityFacadeREST.class);
     }
     
