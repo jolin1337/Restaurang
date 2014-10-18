6fbf40c4187572acd62ad5fd1a5e6b621b0c3398
SimpleCookie <Nanashi1337@gmail.com>
2014-10-18 18:41:42 +0200

    (Desktop) Renamed the print button

--- Desktop/src/se/miun/dt142g/food/MenuPanel.java
+++ Desktop/src/se/miun/dt142g/food/MenuPanel.java
@@ -8,13 +8,9 @@ package se.miun.dt142g.food;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Container;
-import java.awt.Desktop;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener; 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -36,7 +32,6 @@ import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JComboBox;
-import javax.swing.JFileChooser;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.event.PopupMenuEvent;
@@ -95,7 +90,7 @@ public class MenuPanel extends JPanel {
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
         setBackground(Color.white);
         
-        JButton printButton = new JButton("Print PDF");
+        JButton printButton = new JButton("Skriv ut menyblad");
         printButton.addActionListener(new ActionListener() {
 
             @Override
