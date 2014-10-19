d4dd22fb0a882ecbf91ee594bdceeaf1a72847c4
jolin1337 <pj-linden@hotmail.com>
2014-10-19 13:42:48 +0200

    Some more comments and import fixes for the currently edited files in Desktop

--- Desktop/src/se/miun/dt142g/ConfirmationBox.java
+++ Desktop/src/se/miun/dt142g/ConfirmationBox.java
@@ -5,13 +5,21 @@ import javax.swing.JOptionPane;
 
 /**
  * A confimation of when you remove an item in any view we have
+ *
  * @author Ali Omran
  */
 public class ConfirmationBox {
-    
-    static public int confirm(Component a, String s){
+
+    /**
+     * Displays a confirmation box witch displays an userfriendly message when 
+     * user wants to remove an item.
+     * @param a - The component to attach this confirmationbox to
+     * @param s - The identifier of the object to remove
+     * @return 
+     */
+    static public int confirm(Component a, String s) {
         int n = JOptionPane.showConfirmDialog(a, "Ta bort " + s + "?", "Confirm", JOptionPane.YES_NO_OPTION);
         return n;
     }
-    
+
 }
--- Desktop/src/se/miun/dt142g/data/EventPost.java
+++ Desktop/src/se/miun/dt142g/data/EventPost.java
@@ -1,35 +1,68 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * This code is created for one purpose only. And should not be used for any 
+ * other purposes unless the author of this file has apporved. 
+ * 
+ * This code is a piece of a project in the course DT142G on Mid. Sweden university
+ * Created by students for this projekt only
  */
 package se.miun.dt142g.data;
 
-import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+
 /**
+ * This is a dataclass that represents an event (happening).
  *
- * @author Johannes
+ * @author Johannes Lindén
+ * @since 2014-10-04
+ * @version 1.0
  */
 public class EventPost implements Comparable<EventPost> {
+
+    /**
+     * The id (PK on db) To identify this data object
+     */
     private int id;
 
-    private String pubDate="";
-    private String imgSrc="";
-    private String title="";
-    private String description="";
+    /**
+     * The date when this event takes place in a string format
+     */
+    private String pubDate = "";
+    /**
+     * An img name for this post
+     */
+    private String imgSrc = "";
+    /**
+     * The describing title for this eventpost
+     */
+    private String title = "";
+    /**
+     * The description on what happend on the event
+     */
+    private String description = "";
 
-    
+    /**
+     * Constructs an eventpost with a desired id
+     * @param id - The id this post will get
+     */
     public EventPost(int id) {
         this.id = id;
+        
+        // Set the pudDate to current date for now
         this.pubDate = new SimpleDateFormat("yyyyMMdd").format(new Date());
     }
+
+    /**
+     * Constructs an eventpost with all attributes desided
+     * @param id - The identifier for this event
+     * @param pubDate - The startdate for this event
+     * @param imgSrc - The source to image poster
+     * @param title - The titel for this post
+     * @param description - The description of this event
+     */
     public EventPost(int id, String pubDate, String imgSrc, String title, String description) {
         this.id = id;
         this.pubDate = pubDate;
@@ -38,45 +71,94 @@ public class EventPost implements Comparable<EventPost> {
         this.description = description;
     }
 
+    /**
+     * Constructs an simplified eventpost attribute collection 
+     * @param id - The identifier for this event
+     * @param title - The titel for this post
+     * @param description - The description of this event
+     */
     public EventPost(int id, String title, String description) {
         this.id = id;
         this.title = title;
         this.description = description;
         this.pubDate = new SimpleDateFormat("yyyyMMdd").format(new Date());
     }
-    
+
+    /**
+     * gets the id from this event
+     * @return the unique identifier of this post event
+     */
     public int getId() {
         return id;
     }
+    /**
+     * Sets the identifier of this object
+     * @param id - The new id this object retireves
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
 
+    /**
+     * Gets the date when this event takes place
+     * @return the date this event takes place
+     */
     public String getPubDate() {
         return pubDate;
     }
 
+    /**
+     * Setter for start date of this event
+     * @param pubDate 
+     */
     public void setPubDate(String pubDate) {
         this.pubDate = pubDate;
     }
 
+    /**
+     * Getter for the poster image name
+     * @return the image name or sometimes url
+     */
     public String getImgSrc() {
         return imgSrc;
     }
 
+    /**
+     * Setter for image name for this poster
+     * @param imgSrc the image name or sometimes url
+     */
     public void setImgSrc(String imgSrc) {
         this.imgSrc = imgSrc;
     }
 
+    /**
+     * Getter for title of this event that describes what is happening 
+     * @return The title of the event
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * Setter for title in this eventpost
+     * @param title - The title to set this eventpost to have
+     */
     public void setTitle(String title) {
         this.title = title;
     }
 
+    /**
+     * Getter for description
+     * @return the description text
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * Setter for description
+     * @param description - The description this eventpost will have
+     */
     public void setDescription(String description) {
         this.description = description;
     }
@@ -96,43 +178,54 @@ public class EventPost implements Comparable<EventPost> {
             return false;
         }
         final EventPost other = (EventPost) obj;
-        return other.getDescription().equals(getDescription()) && 
-                other.getId() == getId() &&
-                other.getImgSrc().equals(getImgSrc()) && 
-                other.getPubDate().equals(getPubDate()) &&
-                other.getTitle().equals(getTitle());
+        return other.getDescription().equals(getDescription())
+                && other.getId() == getId()
+                && other.getImgSrc().equals(getImgSrc())
+                && other.getPubDate().equals(getPubDate())
+                && other.getTitle().equals(getTitle());
     }
 
+    /**
+     * Converts this EventPost object to an json string
+     * @return An json string representation of this object if it is valid
+     * otherwise empty "{}"
+     */
     public String toJsonString() {
-        
+
         // Set all properties of this event here to export the event to a json object
         JSONObject value = new JSONObject();
         try {
+            // Set ImgSrc
             String imgName = getImgSrc();
-            if(imgName.lastIndexOf("/") > -1)
-                imgName = imgName.substring(getImgSrc().lastIndexOf("/")+1);
-            if(imgName.lastIndexOf("\\") > -1)
-                imgName = imgName.substring(getImgSrc().lastIndexOf("\\")+1);
+            // If it contains slashes remove them here and take the right side 
+            // of the last occurrence of an slahs
+            if (imgName.lastIndexOf("/") > -1) {
+                imgName = imgName.substring(getImgSrc().lastIndexOf("/") + 1);
+            }
+            // If it contains back slashes remove them here and take the right 
+            // side of the last occurrence of an back slahs
+            if (imgName.lastIndexOf("\\") > -1) {
+                imgName = imgName.substring(getImgSrc().lastIndexOf("\\") + 1);
+            }
+            // Insert all attributes in an json object
             value.put("id", getId())
                     .put("image", imgName)
                     .put("pubDate", getPubDate())
                     .put("title", getTitle())
                     .put("description", getDescription());
-        } catch (JSONException ex) {
+        } catch (JSONException ex) { // Nothing to do when something has gone wrong
         }
-        return value.toString();
+        return value.toString(); // return this object representation if it is valid
+                                // otherwise empty "{}"
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
 
     @Override
     public int compareTo(EventPost t) {
         try {
             Long l = Long.parseLong(t.getPubDate());
             return l.compareTo(Long.parseLong(getPubDate()));
-        } catch(NullPointerException | NumberFormatException ex) {
+        } catch (NullPointerException | NumberFormatException ex) {
             return 0;
         }
     }
