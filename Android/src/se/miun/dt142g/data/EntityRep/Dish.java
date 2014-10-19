8179fd1bb25b05ca0343e2d706360b2a20452556
Ulf <Ulf@Ulf-PC>
2014-10-20 00:30:50 +0200

    Android - Added so that you can't order Dishes that have ingredients that are out of stock

--- Android/src/se/miun/dt142g/data/EntityRep/Dish.java
+++ Android/src/se/miun/dt142g/data/EntityRep/Dish.java
@@ -17,14 +17,21 @@ import org.json.JSONObject;
  */
 public class Dish implements Comparable<Dish> {
     private boolean deleted = false;
+    private boolean inStock = false; 
     private int id;
     private String name;
     private float price;
+    private List<Integer> ingredientIds = new ArrayList<Integer>(); 
 
-    public Dish(int id, String name, float price) {
+    public Dish(int id, String name, float price, List<Integer> ingredientIds, boolean inStock) {
         this.id = id;
         this.name = name;
         this.price = price;
+        this.inStock = inStock;
+        for(Integer i : ingredientIds){
+            this.ingredientIds.add(i);
+        }
+        String s = "Fisk";
     }
 
     public boolean isDeleted() {
@@ -80,6 +87,7 @@ public class Dish implements Comparable<Dish> {
         return this.getName() + "\n      " + Float.toString(this.getPrice()) + ":-";
     }
 
+    /*
     public String toJsonString() {
         // Set all properties of this event here to export the event to a json object
         JSONObject value = new JSONObject();
@@ -90,10 +98,24 @@ public class Dish implements Comparable<Dish> {
         } catch (JSONException ex) {
         }
         return value.toString();
-    }
+    }*/
 
     @Override
     public int compareTo(Dish t) {
         return name.toLowerCase().compareTo(t.getName().toLowerCase());
+    }
+
+    /**
+     * @return the inStock
+     */
+    public boolean isInStock() {
+        return inStock;
+    }
+
+    /**
+     * @param inStock the inStock to set
+     */
+    public void setInStock(boolean inStock) {
+        this.inStock = inStock;
     }
 }
