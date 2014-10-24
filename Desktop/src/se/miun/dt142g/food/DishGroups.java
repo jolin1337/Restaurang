bc4081958825fd75797b78b4d56e36eb56adeb19
Nikander <Nikander@Laptop.svl.eduroam.miun.se>
2014-10-24 02:00:37 +0200

    weekly menu in desktop is now in weekday order.

--- Desktop/src/se/miun/dt142g/food/DishGroups.java
+++ Desktop/src/se/miun/dt142g/food/DishGroups.java
@@ -33,8 +33,8 @@ public class DishGroups extends DataSource {
      */
     public List<DishGroup> getDishGroups(String[] groupNames) {
         List<DishGroup> dishesInGroup = new ArrayList<>();
-        for (DishGroup d : dishGroups) {
-            for (String groupName : groupNames) {
+        for (String groupName : groupNames) {
+            for (DishGroup d : dishGroups) {
                 if (d.getGroup().equals(groupName)) {
                     dishesInGroup.add(d);
                     break;
